e1dc00f5523f1fcfe8d73303b2f4fd5783274653
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-08 10:58:35 +0700

    add program menu

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -93,9 +93,24 @@ class SingleLinkdlist {
             cout << endl;
         }
     }
-
 };
 
 int main(){
+    SingleLinkdlist mhs;
+    int nim;
+    char ch;
+    while (1){
+        cout << endl 
+             << "Menu";
+        cout << endl
+             << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukan pilihan (1-5): ";
+        cin >> ch;
+    }   
 
 }
